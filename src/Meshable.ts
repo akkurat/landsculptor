8c32a14d7e9b623c4c5347409993a3b9466b131d
Moritz Vifian <moritz.vifian@gmx.ch>
2020-04-19 08:20:22 +0200

    Fixing Implementation of Ruperts Algorithm
    Adding a few comments for vertice arrays

--- src/Meshable.ts
+++ src/Meshable.ts
@@ -6,31 +6,58 @@ import { Vector2, Vector3 } from "three"
 
 var cdt2d = require('cdt2d')
 
-export type P = { x: number, y: number, edgeIdx?: number }
+export type P = { x: number, y: number }
 export type A3 = [number,number,number]
 export type A2 = [number,number]
 
+
+interface IMeshOptions {
+  minAngle?: number
+  min2CircumferenceRatio?: number
+  minLength?: number
+  maxLength?: number
+  geomTolerance?: number
+}
+
+const defaultMeshOptions: IMeshOptions = {
+  min2CircumferenceRatio: 0.2,
+  geomTolerance: 0.1
+}
 export class Meshable {
   _del: any
   state: string = 'decroach'
   _steinerBlacklist: A3[] = []
+  steinerFinished: boolean = false
+  decroachFinished: boolean = false
 
-  constructor(public _meshTolerance = .2) { }
+  constructor(public meshOptions: IMeshOptions) {
+      this.meshOptions = {...defaultMeshOptions, ...meshOptions}
+   }
 
+  /**Input Vertices */
   _points: P[] = []
 
-  _edges: A2[] = []
-  _allEdges: A2[] = []
+  /**Input segments (PSLG). Rupert refers to the input as segment and to the triangulation lines as edges */
+  _segment: A2[] = []
+  /** Input edges + edges from the convex hull */
+  _allSegments: A2[] = []
 
   _mesh: A3[]
 
+  /** Points inserted to split input segments */
   _antiCroachPoints: P[] = []
+  /** Points inserted to improve triangle quality */
   _steinerPoints: P[] = []
+  // Delaunator uses native arrays for point storage
+  // -> efficiency improvements
   _allPoints: P[] = []
 
   min_length = 5
+  // max_length 
 
-
+  addPoints_(points: A2[], closed=false ) {
+    return this.addPoints( points.map( ([x,y]) => ({x,y}) ), closed )
+  }
   /**
    * add points connected in order of the given array
    * @param points 
@@ -42,22 +69,26 @@ export class Meshable {
     this._points.push(...points)
 
     for (let i = offset; i < this._points.length - 1; i++) {
-      this._edges.push([i, i + 1])
-      this._points[i].edgeIdx = this._edges.length
-      this._points[i + 1].edgeIdx = this._edges.length
+      this.addEdge(i, i+1)
     }
 
     if (closed) {
-      this._edges.push([offset, this._points.length - 1])
-      this._points[offset].edgeIdx = this._edges.length
-      this._points[this._points.length - 1].edgeIdx = this._edges.length
+      this.addEdge( offset, this._points.length-1 )
     }
 
   }
 
-  mesh() {
-    // TODO: for more than one refinement: Track all points
-    this._allPoints = this._points.concat(this._antiCroachPoints,this._steinerPoints)
+  private addEdge(i0: number, i1: number) {
+    this._segment.push([i0, i1])
+  }
+
+  init() {
+    this.mesh()
+    this._allSegments = this.sanitizeHullEdges()
+  }
+
+  private mesh() {
+    this.updatePoints()
 
     this._del = Delaunator.from(this._allPoints.map(p => [p.x, p.y]))
 
@@ -66,16 +97,19 @@ export class Meshable {
     for( let i=0; i<t.length; i+=3 ) {
       this._mesh.push( [t[i],t[i+1],t[i+2]] )
     }
-    this._allEdges = this.sanitizeHullEdges()
 
     // this._mesh = cdt2d(this._allPoints.map(p => [p.x, p.y]))//, this._edges)//, {exterior: false})
   }
-  sanitizeHullEdges(): A2[] {
+  private updatePoints() {
+    this._allPoints = this._points.concat(this._antiCroachPoints, this._steinerPoints)
+  }
+
+  private sanitizeHullEdges(): A2[] {
     const hullPoints = this._del.hull
 
     const e2s = ([i0, i1]: A2): string => i0 + '|' + i1
-    const set = new Set(this._edges.map(e2s))
-    const edges = this._edges.slice()
+    const set = new Set(this._segment.map(e2s))
+    const edges = this._segment.slice()
     for( let i=0; i<hullPoints.length; i++ ) {
       const i0 = hullPoints[i]
       const next = (i + 1) % hullPoints.length 
@@ -91,21 +125,42 @@ export class Meshable {
   }
 
   refineStep(): {seC:P, seL:number, edge:any}[] {
+    let placed
+    if(this.steinerFinished && this.decroachFinished ) {
+      this.state = 'finished'
+    }
     if(this.state == 'steiner') {
-      const placed = this.placeSteiner()
-      // if(!placed || placed.length == 0) 
-        this.state = 'decroach' // Back to decroach
-      // return placed
+      placed = this.placeSteiner()
+      if(!placed || placed.length == 0) 
+        this.steinerFinished = true
+
+      this.state = 'decroach' // Back to decroach
 
     } else if(this.state == 'decroach' ) {
-      const placed = this.decroach()
-      if(!placed || placed.length == 0) 
+      placed = this.decroach()
+      if(!placed || placed.length == 0)  {
         this.state = 'steiner'
-      return placed
-
+        this.decroachFinished = true
+      }
     }
+    this.mesh()
+    return placed
   }
-  placeSteiner(): { seC: P; seL: number, edge: any }[] {
+
+  private splitSegment(segmentIndex: number, newPoint: P) {
+        const toSplitEdge = this._allSegments[segmentIndex]
+        const newPtIndex = this._points.length + this._antiCroachPoints.length
+
+        this._allSegments.push([toSplitEdge[1],newPtIndex])
+        toSplitEdge[1] = newPtIndex
+
+        this._antiCroachPoints.push(newPoint)
+        this.updatePoints()
+
+        // this._allSegments = 
+
+  }
+  private placeSteiner(): { seC: P; seL: number, edge: any }[] {
     
     for( const t of this._mesh ) {
       const tri = Tri.ofArray_(t,this._allPoints)
@@ -113,21 +168,19 @@ export class Meshable {
       if(this.angleCriterion(tri)) {
 
         const {center,r}  = tri.circumference()
-        if( this.isInHull(center) && !this.inBlacklist(t) ) {
-          {
-            const pt = this._allPoints.findIndex(p => this.inCircle(p, center, r))
-            if(pt>-1) {
-              const t = this._mesh.find(t => t.includes(pt))
-              const idx = t.indexOf(pt)
+        // if( this.isInHull(center) && !this.inBlacklist(t) )
+         {
+            for (let [idx,segment] of this._allSegments.entries()) {
+              const [p0,p1] = segment.map(idx => this._allPoints[idx])
+              const pc = Tri.vMean(p0,p1)
+              const l = Tri.distance(p0,p1)
+              if( this.inCircle(center, pc, l/2) ) {
+                this.splitSegment( idx, pc )
+                return [{seC: pc, seL: l, edge: idx}]
+              }
+            }
 
-              const idx0 = t[idx]
-              const p0 = this._allPoints[idx0]
-              const idx1 = t[(idx + 1) % 3]
-              const p1 = this._allPoints[idx1]
 
-              return this.splitAndAdd(p0, p1, t[(idx+2)%3], idx0+'-'+idx1)
-
-            } else {
 
               this._steinerPoints.push(center)
               this.mesh()
@@ -137,47 +190,21 @@ export class Meshable {
               // }
               return [{ seC: center, seL: r * 2, edge: e }]
             }
-          }
-        }
       }
     }
       return []
   }
-  inBlacklist(t: A3) {
-    const pSet = new Set(t)
-    for(let [a,b,c] of this._steinerBlacklist) {
-      if(pSet.has(a) && pSet.has(b) && pSet.has(c) ) {
-        return true;
-      }
-    }
-    return false
+
+  private isBadTriangle(tri: Tri) {
+
   }
-  removeInsertedSteinerPoint(t:A3) {
-
-    const lastIdx = this._allPoints.length-1
-    const triags = this._mesh.filter(t => t.includes(lastIdx))
-
-    // TODO: check on encroaching
-    const badTriangle = triags.map(tri => Tri.ofArray_(tri, this._allPoints))
-      .find(this.angleCriterion)
-    
-    if(badTriangle) {
-      this._steinerPoints.pop()
-      this._steinerBlacklist.push(t)
-      return true;
-    }
-
-
-    return false;
-  }
-
 
   private circumCriterion(tri: Tri) {
     return tri.minLength() < tri.perimeter() * 0.15 || tri.maxLength() > tri.perimeter() * .46
   }
 
   private angleCriterion(tri: Tri) {
-    return tri.minAngle() < 19/180*Math.PI
+    return tri.minAngle() < 33/180*Math.PI
   }
 
   isInHull(p:P) {
@@ -190,28 +217,24 @@ export class Meshable {
     return isInsideCircularPath(p.x, p.y, path)
   }
 
-  splitAndAdd = (p0, p1, thirdPoint, edge) => {
+  splitAndAdd = (p0, p1, edgeIdx, edge) => {
     const seC = Tri.vMean(p0, p1)
     const seL = Tri.distance(p0, p1)
-    if (this.min_length > seL) {
-      return []
-    }
+    // if (this.min_length > seL) {
+    //   return []
+    // }
 
-    const p2 = this._allPoints[thirdPoint]
+    const inCircum = (p, idx) => this.inCircle(p, seC, seL/2)
 
-    
+    const tol = this.meshOptions.geomTolerance
 
-
-    const inCircum = (p, idx) =>
-      // idx != thirdPoint && 
-      this.inCircle(p, seC, seL/2)
-
-    const close = ({ x, y }) => Math.abs(x - seC.x) < this._meshTolerance
-      && Math.abs(y - seC.y) < this._meshTolerance
+    const close = ({ x, y }) => Math.abs(x - seC.x) < tol
+      && Math.abs(y - seC.y) < tol
 
     const p01 = Tri.vSub(p1,p0)
 
     const res = this._allPoints.filter(inCircum)
+
     if (res.length) {
 
       const closePoints = res.filter(close)
@@ -223,8 +246,9 @@ export class Meshable {
       console.log(alignedPoint)
 
       if (!alignedPoint && closePoints.length == 0) {
-        this._antiCroachPoints.push(seC)
         const placed = []
+        
+        this.splitSegment( edgeIdx, seC )
         // placed.push(...splitAndAdd(p0, seC) )
         // placed.push(...splitAndAdd(seC, p1) )
         placed.push({ seC, seL, edge })
@@ -240,7 +264,7 @@ export class Meshable {
   }
 
   private inCircle(p: any, seC: { x: number; y: number }, r: number) {
-    return Math.sqrt((p.x - seC.x) ** 2 + (p.y - seC.y) ** 2) < r - this._meshTolerance
+    return Math.sqrt((p.x - seC.x) ** 2 + (p.y - seC.y) ** 2) < r - this.meshOptions.geomTolerance
   }
 
   decroach() {
@@ -251,11 +275,11 @@ export class Meshable {
     //  return self.indexOf(value) === index;
     //  }
 
-    const richPoint = (p0, p1, idx0, idx1, other?) => ({ l: Tri.distance(p0, p1), p0, p1, idx0, idx1, other })
-    const presentEdges = this._allEdges.map( ([idx0,idx1]) => {
+    const richPoint = (p0, p1, idx0, idx1, segmentIdx) => ({ l: Tri.distance(p0, p1), p0, p1, idx0, idx1, segmentIdx})
+    const presentEdges = this._allSegments.map( ([idx0,idx1], idx) => {
       const p0 = this._allPoints[idx0]
       const p1 = this._allPoints[idx1]
-      return richPoint(p0, p1, idx0, idx1)
+      return richPoint(p0, p1, idx0, idx1, idx)
     } );
     // this._mesh.forEach((idxs) => {
 
@@ -273,9 +297,9 @@ export class Meshable {
     // const presentEdges = []
     // this.forEachTriangleEdge( (e,p0,p1) => presentEdges.push({e,p0,p1}))
 
-    for (const {p0,p1,idx0,idx1,other} of presentEdges) {
+    for (const {p0,p1,idx0,idx1,segmentIdx} of presentEdges) {
 
-      const placed = this.splitAndAdd(p0, p1, other, idx0+"<|-"+other+"-|>"+idx1)
+      const placed = this.splitAndAdd(p0, p1, segmentIdx, idx0+"<|-"+segmentIdx+"-|>"+idx1)
       if (placed.length > 0) { // splitting has been done -> remesh
         return placed
       }
--- src/refinement.ts
+++ src/refinement.ts
@@ -11,142 +11,148 @@ import { PaperZoom } from "./PaperZoom";
 import { PaperScope } from "paper/dist/paper-core";
 
 async function run() {
-    var paperCanvas = document.getElementById('refinement_paper').appendChild(document.createElement('canvas'));
-    // Create an empty project and a view for the canvas:
-    const ps = new PaperScope()
-    ps.setup(paperCanvas);
-    new PaperZoom(ps.project)
+  var paperCanvas = document.getElementById('refinement_paper').appendChild(document.createElement('canvas'));
+  // Create an empty project and a view for the canvas:
+  const ps = new PaperScope()
+  ps.setup(paperCanvas);
+  new PaperZoom(ps.project)
 
-    const meshable = new Meshable()
+  const meshable = new Meshable({})
 
-//   meshable.addPoints( circle(50, 100, 50, -10 ), true )
-  meshable.addPoints( circle(150, 100, 20, 50), true )
-  meshable.addPoints( line(new Vector3(), new Vector3(200,50), 30)  )
-  meshable.addPoints( line_(50,170,160,140,5) )
-//   meshable.addPoints( line(new Vector3(0,200), new Vector3(200,250), 30)  )
-  meshable.addPoints( circle(20, 20, 33, 23 ), true )
 
-//   ps.project.activeLayer.fitBounds( ps.view.bounds )
 
-  meshable.mesh()
+  meshable.addPoints(circle(50, 50, 33, 23), true)
+  meshable.addPoints([{ x: 0, y: 0 }, { x: 0, y: 300 }, { x: 300, y: 300 }, { x: 300, y: 0 }], true)
+  meshable.addPoints_([[50, 150], [50, 100], [120, 120]], true)
+  //   meshable.addPoints( circle(50, 100, 50, -10 ), true )
+  // meshable.addPoints( circle(150, 100, 20, 50), true )
+  // meshable.addPoints( line(new Vector3(), new Vector3(200,50), 30)  )
+  meshable.addPoints(line_(50, 170, 160, 140, 5).concat(line_(160, 140, 170, 293, 12)))
+  //   meshable.addPoints( line(new Vector3(0,200), new Vector3(200,250), 30)  )
 
-    const play = document.getElementById("play_auto") as HTMLInputElement
-    const btn = document.getElementById("btn_play") as HTMLButtonElement
+  //   ps.project.activeLayer.fitBounds( ps.view.bounds )
 
-    btn.addEventListener('click', ev => step() )
+  meshable.init()
+  draw()
+
+  const play = document.getElementById("play_auto") as HTMLInputElement
+  const btn = document.getElementById("btn_play") as HTMLButtonElement
+
+  btn.addEventListener('click', ev => step())
 
 
   const step = () => {
-          ps.project.activeLayer.removeChildren()
-          draw()
-          const color = new Color(meshable.state == 'steiner' ?'lightgreen':'magenta')
-          const placeds = meshable.refineStep()
-          //   meshable.refineMultiple(5)
-          meshable.mesh()
-          if(placeds && placeds.length) {
-            for(const placed of placeds) {
-            const p = new ps.Point(placed.seC.x, placed.seC.y);
-            const c = new ps.Path.Circle(p, placed.seL/2);
-            c.strokeColor = color
-            const vertex = new ps.Path.Circle(p, 1);
-            vertex.strokeColor = color
-            const text = new ps.PointText(p)
-            text.content = placed.edge
-            }
-          } else {
-              if(meshable.state == 'finished') {
-                  alert('fin')
-
-              }
-          }
-        ps.project.activeLayer.fitBounds( ps.view.bounds )
-          
-      if(play.checked && meshable.state != 'finished') {
-          setTimeout(step, 50)
+    ps.project.activeLayer.removeChildren()
+    const color = new Color(meshable.state == 'steiner' ? 'lightgreen' : 'magenta')
+    const placeds = meshable.refineStep()
+    //   meshable.refineMultiple(5)
+    draw()
+    if (placeds && placeds.length) {
+      for (const placed of placeds) {
+        const p = new ps.Point(placed.seC.x, placed.seC.y);
+        const c = new ps.Path.Circle(p, placed.seL / 2);
+        c.strokeColor = color
+        const vertex = new ps.Path.Circle(p, 1);
+        vertex.strokeColor = color
+        const text = new ps.PointText(p)
+        text.content = placed.edge
       }
+    } else {
+      if (meshable.state == 'finished') {
+        alert('fin')
+
+      }
+    }
+    // ps.project.activeLayer.fitBounds( ps.view.bounds )
+
+    if (play.checked && meshable.state != 'finished') {
+      setTimeout(step, 1)
+    }
   }
 
   function draw() {
-      drawPoints(meshable._points, '#ff0000A0');
-      drawPoints(meshable._antiCroachPoints, '#0000ff50');
-      drawPoints(meshable._steinerPoints, '#00ff5050');
-  drawPointTexts(meshable._allPoints, 'green');
-  drawEdges(meshable._allEdges, '#ff0000A0');
+    meshable.forEachTriangleEdge((e, p, q) => {
 
-  const cifs = [], newPoints = []
-
-//   for (let t of meshable._mesh) {
-//     const tri = Tri.ofArray(t[0], t[1], t[2], meshable._allPoints )
-//     const path = new ps.Path()
-//     path.strokeColor = new Color('black')
-//     path.fillColor = nextColor()
-//     path.fillColor.alpha = 0.4
-//     drawTriangle(tri, path);
-    
-//     const cif = tri.circumference()
-//     cifs.push(cif)
-//     if( cif.r < 300 && (tri.minLength() < tri.perimeter()/5 || tri.maxLength() > .45*tri.perimeter() ) ) {
-//       // The height info is not trivial. Damn
-//       const p = cif.center
-//       newPoints.push(p)
-//     // ps.view.draw();
-//     }
-//   }
-    meshable.forEachTriangleEdge( (e,p,q) => {
-
-        const path = new ps.Path.Line(new Point(p.x, p.y), new Point(q.x,q.y) )
-        path.strokeColor = new Color('black')
-        const tp = new Point(Tri.vMean(p,q))
-        // const text =new ps.PointText(tp)
-        // text.fontSize = '3px'
-        // text.content = e
+      const path = new ps.Path.Line(new Point(p.x, p.y), new Point(q.x, q.y))
+      path.strokeColor = new Color('black')
+      const tp = new Point(Tri.vMean(p, q))
+      // const text =new ps.PointText(tp)
+      // text.fontSize = '3px'
+      // text.content = e
     })
+    drawPoints(meshable._points, '#ff0000A0');
+    drawPoints(meshable._antiCroachPoints, '#0000ff50');
+    drawPoints(meshable._steinerPoints, '#00ff5050');
+    drawPointTexts(meshable._allPoints, 'green');
+    drawEdges(meshable._allSegments, '#ff0000A0');
+
+    const cifs = [], newPoints = []
+
+    //   for (let t of meshable._mesh) {
+    //     const tri = Tri.ofArray(t[0], t[1], t[2], meshable._allPoints )
+    //     const path = new ps.Path()
+    //     path.strokeColor = new Color('black')
+    //     path.fillColor = nextColor()
+    //     path.fillColor.alpha = 0.4
+    //     drawTriangle(tri, path);
+
+    //     const cif = tri.circumference()
+    //     cifs.push(cif)
+    //     if( cif.r < 300 && (tri.minLength() < tri.perimeter()/5 || tri.maxLength() > .45*tri.perimeter() ) ) {
+    //       // The height info is not trivial. Damn
+    //       const p = cif.center
+    //       newPoints.push(p)
+    //     // ps.view.draw();
+    //     }
+    //   }
 
 
 
-  ps.view.update();
-}
+    ps.view.update();
+  }
 
   // // points3d.push( ...line(new Vector3(),new Vector3(-20,30,20), 20 ) )
   // // points3d.push( ...line(new Vector3(),new Vector3(20,30,-40), 20 ) )
-//   points3d.push( ...line(new Vector3(100,40,130),new Vector3(100, 0,30), 20 ) )
-//   points3d.push( ...line(new Vector3(100,40,-100),new Vector3(100, 0,30), 20 ) )
-//   points3d.push( ...line(new Vector3(-50,0,0),new Vector3(0,0,200), 20 ) )
-    
+  //   points3d.push( ...line(new Vector3(100,40,130),new Vector3(100, 0,30), 20 ) )
+  //   points3d.push( ...line(new Vector3(100,40,-100),new Vector3(100, 0,30), 20 ) )
+  //   points3d.push( ...line(new Vector3(-50,0,0),new Vector3(0,0,200), 20 ) )
 
 
-    function drawPoints( points: P[], color: string, size = 1) {
-        for (let p of points) {
-            const vertex = new ps.Path.Circle(new ps.Point(p.x, p.y), size);
-            vertex.fillColor = new Color(color);
-        }
+
+  function drawPoints(points: P[], color: string, size = 1) {
+    for (let p of points) {
+      const vertex = new ps.Path.Circle(new ps.Point(p.x, p.y), size);
+      vertex.fillColor = new Color(color);
     }
-    function drawEdges( edges: A2[], color: string, size = 1) {
-        for (let [i0,i1] of edges) {
-          const p0 = meshable._allPoints[i0]
-          const p1 = meshable._allPoints[i1]
-            const line = new ps.Path.Line(new ps.Point(p0.x, p0.y), new ps.Point(p1.x, p1.y));
-            line.strokeColor = new Color(color);
-            line.strokeWidth=4
-        }
+  }
+  function drawEdges(edges: A2[], color: string, size = 1) {
+    const col = new Color(color);
+    for (let [i0, i1] of edges) {
+      const p0 = meshable._allPoints[i0]
+      const p1 = meshable._allPoints[i1]
+      const line = new ps.Path.Line(new ps.Point(p0.x, p0.y), new ps.Point(p1.x, p1.y));
+      line.strokeColor = new Color(col)
+      col.hue += 122
+      line.strokeWidth = 2
     }
-    function drawPointTexts( points: P[], color: string, size = 1) {
-        for (let [idx,p] of points.entries()) {
-        const text =new ps.PointText(new Point(p))
-        text.fontSize = '3px'
-        text.content = ""+idx
-        }
+  }
+  function drawPointTexts(points: P[], color: string, size = 1) {
+    for (let [idx, p] of points.entries()) {
+      const text = new ps.PointText(new Point(p))
+      text.fontSize = '3px'
+      text.content = "" + idx
     }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    run();
+  run();
 })
 
 let lastColor = new Color('hsl(180deg,80%,50%')
 
 function nextColor() {
-    lastColor = new Color(lastColor)
-    lastColor.hue += 30 
-    return lastColor
+  lastColor = new Color(lastColor)
+  lastColor.hue += 30
+  return lastColor
 }
