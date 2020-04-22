eb2ad36c3b7891d62fe47447bcbd36678c1ce8ff
Moritz Vifian <moritz.vifian@gmx.ch>
2020-04-22 14:00:52 +0200

    Adding point distance function

--- src/Sanitzing.ts
+++ src/Sanitzing.ts
@@ -1,5 +1,6 @@
 // var cleanPSLG = require('clean-pslg')
 import * as cleanPSLG from 'clean-pslg'
+import { A2 } from './Meshable';
 
 /**
  * Merges two points of the *same* segment if their distance is < minDistance
@@ -9,11 +10,11 @@ import * as cleanPSLG from 'clean-pslg'
  * @param minDistance 
  * inplace
  */
-export function mergePointsSameSegment(vertices,segments, minDistance) {
-    
+export function mergePointsSameSegment(vertices, segments, minDistance) {
+
 }
 
-export function increaseDistance(vertices,segments, minDistance) {
+export function increaseDistance(vertices, segments, minDistance) {
 
 }
 
@@ -21,7 +22,7 @@ export function mergePointsIntoSegment(vertices: number[][], segments: number[][
 
     // return cleanPSLG(vertices, segments)
 
-    for( const [idx,[i0,i1]] of segments.entries() ) {
+    for (const [idx, [i0, i1]] of segments.entries()) {
         const p0 = vertices[i0], p1 = vertices[i1]
 
 
@@ -30,4 +31,89 @@ export function mergePointsIntoSegment(vertices: number[][], segments: number[][
 
     // todo
     return false;
+}
+
+export function findClosePoints(vertices: A2[], segments: number[][], minDistance: number) {
+    for (const [idx, [i0, i1]] of segments.entries()) {
+        const p0 = vertices[i0], p1 = vertices[i1]
+
+        for (let i = 0; i < vertices.length; i++) {
+            if (i != i0 && i != i1) {
+                const p = vertices[i]
+                const l = distanceToPoint(p0, p1, p)
+
+            }
+
+        }
+
+
+
+    }
+}
+
+
+/**
+ * TODO: Numererically better implementation
+ * -> if distance(p0, p1) >> L this will become very unprecise
+ * One idea: contract Points on line closer to target point
+ * (probably iterative)
+ * @param p0 Point of Line segment 0 
+ * @param p1 Point of Line segment 1 
+ * @param p Point close to segment
+ * 
+ * https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points
+ */
+export function distanceToPoint([x1, y1]: A2, [x2, y2]: A2, [xp, yp]: A2, refine = false) {
+    const dx21 = x2 - x1
+    const dy21 = y2 - y1
+
+    const l12sq = dx21 ** 2 + dy21 ** 2;
+
+
+    const denom = Math.sqrt(l12sq);
+    // Not taking abs could give info about orientation
+    // Could be interesting in order to contract always towards the segment if p is an endpoint of that segment
+    const nom = Math.abs(dy21 * xp - dx21 * yp + x2 * y1 - y2 * x1)
+    const l = nom / denom
+
+    if (refine) {
+        const dxp1 = xp - x1;
+        const dyp1 = yp - y1;
+        const l1psq = (dxp1) ** 2 + (dyp1) ** 2
+        const dxp2 = xp - x2;
+        const dyp2 = yp - y2;
+        const l2psq = (dxp2) ** 2 + (dyp2) ** 2
+
+        if (l12sq > 1e3 * l) {
+
+            const sq = Math.sqrt
+            const ñ = [dx21, dy21].map(v => v / sq(l12sq))
+
+            // dx = dir_x * dl
+            // -> dl = dx/dir_x
+
+            const l1p = sq(l1psq)
+            const l2p = sq(l2psq)
+
+
+
+            const closePoint1: A2 = [x1+ñ[0]*l1p, y1+ñ[1]*l1p]
+            const pertub = closePoint1[0] - xp
+            const closePoint2: A2 = [closePoint1[0]+ñ[0]*pertub, closePoint1[1]+ñ[1]*pertub]
+
+            // const dy2 = dxp2 / nDir[0] * nDir[1]
+            // const dx2 = dyp2 / nDir[1] * nDir[0]
+
+            // const closePoint1_: A2 = [xp, y2 + dy2]
+            // const closePoint2_: A2 = [x2 + dx2, yp]
+
+
+            return distanceToPoint(closePoint1, closePoint2, [xp, yp])
+
+        }
+
+    }
+
+    return l;
+
 }
--- test/tests.ts
+++ test/tests.ts
@@ -1,5 +1,11 @@
 import * as assert from 'assert'
-import { mergePointsIntoSegment } from '../src/Sanitzing'
+import { mergePointsIntoSegment, distanceToPoint } from '../src/Sanitzing'
+import { use } from 'chai';
+
+const  chai = require('chai');
+const  chaiAlmost = require( 'chai-almost');
+
+use(chaiAlmost());
 
 // describe('Array', function() {
 //   describe('#indexOf()', function() {
@@ -9,20 +15,67 @@ import { mergePointsIntoSegment } from '../src/Sanitzing'
 //   });
 // });
 
-describe('MergeToSegment', function () {
-  describe('SimpleClosePoint', function () {
+// describe('MergeToSegment', function () {
+//   describe('SimpleClosePoint', function () {
 
-    const points = [[0, 0], [2, 0], [1, 1], [1, 0.1]]
-    const segments = [[0, 1], [2, 3]]
-    const changed = mergePointsIntoSegment(points, segments, 0.1)
+//     const points = [[0, 0], [2, 0], [1, 1], [1, 0.1]]
+//     const segments = [[0, 1], [2, 3]]
+//     const changed = mergePointsIntoSegment(points, segments, 0.1)
 
-    const expectedPoints = [[0, 0], [2, 0], [1, 1], [1, 0]]
-    const expectedSegments = [[0, 3], [2, 3], [3,2]]
+//     const expectedPoints = [[0, 0], [2, 0], [1, 1], [1, 0]]
+//     const expectedSegments = [[0, 3], [2, 3], [3,2]]
 
-    console.log(changed)
+//     console.log(changed)
 
-    it('should return -1 when the value is not present', function () {
-      assert.equal(points, segments)
-    });
+//     it('should return -1 when the value is not present', function () {
+//       assert.equal(points, segments)
+//     });
+//   })
+// })
+
+
+describe('Distance2Line', function () {
+  describe('Square', function () {
+
+    const l = distanceToPoint([1,0], [0,1],[1,1])
+    it('sqrt(2)/2 is expected', () => chai.expect(l).to.almost(Math.SQRT1_2) )
+  })
+  describe('Ortho', function () {
+
+    const l = distanceToPoint([0,0], [0,1],[0.5,0.5])
+      it('l=0.5', () => assert.equal(l, 0.5) )
+  })
+  describe('Diagonal', function () {
+
+    const l = distanceToPoint([-3, 3], [5,-5],[Math.SQRT1_2,Math.SQRT1_2])
+      it('l=1', () => assert.equal(l, 1) )
+  })
+  describe('LargeNumberOrtho', function () {
+
+    const l = distanceToPoint([-1e6,5e20], [1e7,5e20],[0,4e20])
+      it(' l=1e20', () => assert.equal(l, 1e20) )
+  })
+  describe('LargeNumberDiagonal', function () {
+    const l = diagonal(5e3, Math.SQRT1_2, Math.SQRT1_2)
+      it('l=1', () => assert.equal(l, 1) )
+  })
+  describe('LargeNumberDiagonal Trhough (1|1)', function () {
+    const l = diagonalThrough(5e3, 1, 1 )
+      it('l=0', () => assert.equal(l, 0) )
+  })
+  describe('LargeNumberDiagonal Refined', function () {
+    const l = diagonal(5e25, Math.SQRT1_2, Math.SQRT1_2, true)
+      it('l=1', () => assert.equal(l, 1) )
+  })
+  describe('LargeNumberDiagonal', function () {
+    const l = diagonal(1e10, Math.SQRT1_2, Math.SQRT1_2, false)
+      it('l=1', () => assert.equal(l, 1) )
   })
 })
+
+function diagonal(a, x, y, refine=false) {
+  return distanceToPoint([-a, a], [a, -a], [x, y], refine);
+}
+function diagonalThrough(a, x, y) {
+  return distanceToPoint([-a+x, a+y], [a+x, -a+y], [x, y]);
+}
